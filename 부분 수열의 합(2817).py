c74b4c43f199b6f40ab6fb78071c6c187cc281ea
jiohjung98 <ckaquz98@naver.com>
2023-11-03 17:43:21 +0900

    부분 수열의 합(2817)

---
+++ 부분 수열의 합(2817).py
@@ -0,0 +1,19 @@
+# SWEA D# 2817번 부분 수열의 합
+
+from itertools import combinations
+
+T = int(input())
+
+for i in range(1, T+1):
+    N, K = map(int, input().split())
+    arr = list(map(int, input().split()))
+    
+    answer = 0
+    cnt = 0
+            
+    for j in range(1, N+1):
+        for value in combinations(arr, j):
+            answer = sum(value)
+            if answer == K:
+                cnt += 1
+            
